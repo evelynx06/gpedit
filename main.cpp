176651b9b9d59a7e029ab516593345291a96a922
Evelyn <evelyn.lindelof@gmail.com>
2025-08-28 10:45:14 +0200

    changed bracket style

--- main.cpp
+++ main.cpp
@@ -3,37 +3,32 @@
 
 using namespace std;
 
-unsigned char gp_read_byte(ifstream &fileStream)
-{
+unsigned char gp_read_byte(ifstream &fileStream) {
 	char buffer[1];
 	fileStream.read(buffer, sizeof(buffer));
 	return (unsigned char)buffer[0];
 }
 
-char gp_read_signedbyte(ifstream &fileStream)
-{
+char gp_read_signedbyte(ifstream &fileStream) {
 	char buffer[1];
 	fileStream.read(buffer, sizeof(buffer));
 	return buffer[0];
 }
 
-bool gp_read_bool(ifstream &fileStream)
-{
+bool gp_read_bool(ifstream &fileStream) {
 	char buffer[1];
 	fileStream.read(buffer, sizeof(buffer));
 	return (bool)buffer[0];
 }
 
-unsigned short gp_read_short(ifstream &fileStream)
-{
+unsigned short gp_read_short(ifstream &fileStream) {
 	char buffer[2];
 	fileStream.read(buffer, sizeof(buffer));
 	return (unsigned char)(buffer[0]) |
 			 (unsigned char)(buffer[1]) << 8;
 }
 
-unsigned int gp_read_int(ifstream &fileStream)
-{
+unsigned int gp_read_int(ifstream &fileStream) {
 	char buffer[4];
 	fileStream.read(buffer, sizeof(buffer));
 	return (unsigned char)(buffer[0]) |
@@ -42,8 +37,7 @@ unsigned int gp_read_int(ifstream &fileStream)
 			 (unsigned char)(buffer[3]) << 24;
 }
 
-string gp_read_bytestring(ifstream &fileStream)
-{
+string gp_read_bytestring(ifstream &fileStream) {
 	size_t length = gp_read_byte(fileStream);
 	char buffer[length + 1];
 	fileStream.read(buffer, length);
@@ -51,8 +45,7 @@ string gp_read_bytestring(ifstream &fileStream)
 	return buffer;
 }
 
-string gp_read_intstring(ifstream &fileStream)
-{
+string gp_read_intstring(ifstream &fileStream) {
 	size_t length = gp_read_int(fileStream);
 	char buffer[length + 1];
 	fileStream.read(buffer, length);
@@ -60,13 +53,11 @@ string gp_read_intstring(ifstream &fileStream)
 	return buffer;
 }
 
-string gp_read_intbytestring(ifstream &fileStream)
-{
+string gp_read_intbytestring(ifstream &fileStream) {
 	size_t lengthInt = gp_read_int(fileStream);
 	size_t lengthByte = gp_read_byte(fileStream);
 	
-	if (lengthInt != lengthByte + 1)
-	{
+	if (lengthInt != lengthByte + 1) {
 		cerr << "Error: Mismatched string lengths in IntByteString.";
 		return "";
 	}
@@ -77,13 +68,11 @@ string gp_read_intbytestring(ifstream &fileStream)
 	return buffer;
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
 	string filePath = argc == 2 ? argv[1] : "test.gp3";
 	
 	ifstream fileStream(filePath, ios::in|ios::binary);
-	if (!fileStream)
-	{
+	if (!fileStream) {
 		cerr << "Error opening file.";
 		return 1;
 	}
@@ -91,8 +80,7 @@ int main(int argc, char const *argv[])
 	string version = gp_read_bytestring(fileStream);
 	fileStream.seekg(30 - version.length(), ifstream::cur);
 	
-	if (version != "FICHIER GUITAR PRO v3.00")
-	{
+	if (version != "FICHIER GUITAR PRO v3.00") {
 		cerr << "Incompatible file format '" << version << "'" << endl;
 		return 1;
 	}
