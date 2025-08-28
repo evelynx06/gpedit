ec6c1a1a8bd8a56273607d8bf0719d0d296a1e7a
Evelyn <evelyn.lindelof@gmail.com>
2025-08-28 15:54:05 +0200

    GPFile data structure now includes measure headers

--- GPFile.cpp
+++ GPFile.cpp
@@ -14,8 +14,28 @@ struct midiChannel {
 	unsigned char blank2;
 };
 
-struct measure {
-	
+enum MeasureHeaderFlags {
+	KEYSIG_NUMERATOR = 0x01,
+	KEYSIG_DENOMINATOR = 0x02,
+	REPEAT_BEGIN = 0x04,
+	REPEAT_END = 0x08,
+	ALTEND_NUMBER = 0x10,
+	MARKER = 0x20,
+	TONALITY = 0x40,
+	DOUBLE_BAR = 0x80
+};
+
+struct measureHeader {
+	unsigned char flags;		// indicates what data is present in the measure header
+									// REPEAT_BEGIN and DOUBLE_BAR don't have any associated data
+	unsigned char keysigNumerator;
+	unsigned char keysigDenominator;
+	unsigned char repeatEnd;	// number of repeats
+	unsigned char altendNumber;
+	string markerName;
+	unsigned char markerColor[4];	// red, green, blue, white (white is always 0)
+	unsigned char tonalityRoot;	// key change: key signature root
+	unsigned char tonalityType;	// key change: key signature type
 };
 
 class GPFile {
@@ -41,6 +61,8 @@ class GPFile {
 		
 		int measureCount;
 		int trackCount;
+		
+		measureHeader measureHeaders[];
 		
 		GPFile() {
 			
