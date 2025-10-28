f6a4b3b68fdb220b14c8fba82cfeb68c1ea50eee
Evelyn <evelyn.lindelof@gmail.com>
2025-10-28 18:12:42 +0100

    better project structure and makefile

--- gp_read.cpp
+++ gp_read.cpp
@@ -4,38 +4,38 @@
 #include "gp_read.hpp"
 
 namespace gp_read {
-	unsigned char _read_byte(std::ifstream &fileStream) {
+	unsigned char read_byte(std::ifstream &fileStream) {
 		char buffer[1];
 		fileStream.read(buffer, sizeof(buffer));
 		return (unsigned char)buffer[0];
 	}
 	
-	char _read_signedbyte(std::ifstream &fileStream) {
+	char read_signedbyte(std::ifstream &fileStream) {
 		char buffer[1];
 		fileStream.read(buffer, sizeof(buffer));
 		return buffer[0];
 	}
 	
-	bool _read_bool(std::ifstream &fileStream) {
+	bool read_bool(std::ifstream &fileStream) {
 		char buffer[1];
 		fileStream.read(buffer, sizeof(buffer));
 		return (bool)buffer[0];
 	}
 	
-	short _read_short(std::ifstream &fileStream) {
+	short read_short(std::ifstream &fileStream) {
 		char buffer[2];
 		fileStream.read(buffer, sizeof(buffer));
 		return (unsigned char)(buffer[0]) | (unsigned char)(buffer[1]) << 8;
 	}
 	
-	int _read_int(std::ifstream &fileStream) {
+	int read_int(std::ifstream &fileStream) {
 		char buffer[4];
 		fileStream.read(buffer, sizeof(buffer));
 		return (unsigned char)(buffer[0]) | (unsigned char)(buffer[1]) << 8 |
 				 (unsigned char)(buffer[2]) << 16 | (unsigned char)(buffer[3]) << 24;
 	}
 	
-	std::string _read_bytestring(std::ifstream &fileStream) {
+	std::string read_bytestring(std::ifstream &fileStream) {
 		int length = read_byte(fileStream);
 		char buffer[length + 1];
 		fileStream.read(buffer, length);
@@ -43,7 +43,7 @@ namespace gp_read {
 		return buffer;
 	}
 	
-	std::string _read_intstring(std::ifstream &fileStream) {
+	std::string read_intstring(std::ifstream &fileStream) {
 		int length = read_int(fileStream);
 		char buffer[length + 1];
 		fileStream.read(buffer, length);
@@ -51,7 +51,7 @@ namespace gp_read {
 		return buffer;
 	}
 	
-	std::string _read_intbytestring(std::ifstream &fileStream) {
+	std::string read_intbytestring(std::ifstream &fileStream) {
 		int lengthInt = read_int(fileStream);
 		int lengthByte = read_byte(fileStream);
 
--- makefile
+++ makefile
@@ -1,10 +1,36 @@
 ifeq ($(OS), Windows_NT)
-	curseslib := pdcurses
+	CURSESLIB := pdcurses
 else
-	curseslib := ncurses
+	CURSESLIB := ncurses
 endif
 
-all: gpedit
+BUILD_DIR = build
+OBJ_DIR = $(BUILD_DIR)/obj
 
-gpedit: main.cpp
-	g++ main.cpp -l$(curseslib) -o ./build/gpedit
+OBJS = $(OBJ_DIR)/main.o \
+		 $(OBJ_DIR)/gp_read.o \
+		 $(OBJ_DIR)/gp_file.o
+		 
+LIBS = -l$(CURSESLIB)
+CFLAGS = -O -Wall
+EXEC = $(BUILD_DIR)/gpedit
+
+
+.PHONY: all clean
+
+all: $(EXEC)
+
+clean:
+	rm -r $(BUILD_DIR)
+
+$(EXEC): $(OBJS)
+	@mkdir -p $(BUILD_DIR)
+	g++ $(OBJS) $(LIBS) -o $(EXEC)
+
+$(OBJ_DIR)/%.o: %.cpp
+	@mkdir -p $(OBJ_DIR)
+	g++ $(CFLAGS) -c -o $@ $<
+
+$(OBJ_DIR)/main.o: main.cpp gp_file.hpp
+$(OBJ_DIR)/gp_read.o: gp_read.cpp gp_read.hpp
+$(OBJ_DIR)/gp_file.o: gp_file.cpp gp_file.hpp gp_read.hpp
